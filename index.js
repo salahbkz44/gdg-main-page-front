e4e2bc53a78acc2072188c2ab41c9e1c0e643f34
salahbkz44 <117542403+salahbkz44@users.noreply.github.com>
2023-04-14 22:37:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,23 +3,27 @@
 let query=window.matchMedia("(max-width:767px)");
 if(query.matches)
 {
+    $('.1').text("International women");
+    $('.1').after("<h1 class='2'>day event</h1>");
+    $('.col-4').attr("class","col-12")
+
     $('.home').click(function () {
-        $('.click').animate({ left: '4.5vw', bottom: '-0vw' })
+        $('.click').animate({ left: '4.5vw', bottom: '-0.7vw' })
         home()
     })
     $('.event').click(function () {
-        $('.click').animate({ left: '19.5vw', bottom: '-0vw' })
+        $('.click').animate({ left: '19.5vw', bottom: '-0.7vw' })
         events()
 
     })
     $('.contact').click(function () {
-        $('.click').animate({ left: '34.5vw', bottom: '-0vw' })
+        $('.click').animate({ left: '34.5vw', bottom: '-0.7vw' })
 
         contact()
 
     })
     $('.heart').click(function () {
-        $('.click').animate({ left: '49.5vw',bottom:'-0vw'})
+        $('.click').animate({ left: '49.5vw',bottom:'-0.7vw'})
         heart()
 
     })
@@ -117,6 +121,18 @@ if(query.matches)
         $('.contact img').attr("src", "images/contact-icon-white.png")
         $('.heart img').attr("src", "images/heart-icon-blue.png")
     }
+
+    function heart() {
+        $('.heart').attr("class", "clicked heart nav-links");
+        $('.event').attr("class", "event nav-links");
+        $('.contact').attr("class", "contact nav-links");
+        $('.home').attr("class", "home nav-links");
+        $('.home img').attr("src", "images/home-icon-blue.png")
+        $('.event img').attr("src", "images/event-icon-blue.png")
+        $('.contact img').attr("src", "images/contact-icon-blue.png")
+        $('.heart img').attr("src", "images/heart-icon-white.png")
+    }
+}
 
     function heart() {
         $('.heart').attr("class", "clicked heart nav-links");
